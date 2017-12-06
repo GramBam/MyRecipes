8f31c5e77414fe04b236c0f000beef32ae63eddc
GramBam <graham.moss@hotmail.com>
2017-12-06 03:58:59 -0500

    create recipe controller, seed database, implement gravatar

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+chef = Chef.create(chefname: 'Joe', email: 'joe@example.com')
+recipes = chef.recipes.create([{ name: 'pesto', summary: "very tasty", description: "This will teach you how to make pesto"}, { name: 'chicken', summary: "gosh darn delicious", description: "This will teach you how to make chicken" }])
