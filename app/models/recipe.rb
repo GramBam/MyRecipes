4d6cf2d4c3bcc87497714ab9d40da680aef4b752
GramBam <graham.moss@hotmail.com>
2017-12-06 15:18:08 -0500

    Add recipe picture upload logic & validations

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,4 +4,14 @@ class Recipe < ActiveRecord::Base
   validates :name, presence: true, length: { minimum: 3, maximum: 100 }
   validates :summary, presence: true, length: { minimum: 8, maximum: 150 }
   validates :description, presence: true, length: { minimum: 15, maximum: 500 }
+  mount_uploader :picture, PictureUploader
+  validate :picture_size
+
+  private
+
+  def picture_size
+    if picture.size > 5.megabytes
+      errors.add(:picture, "should be less than 5MB")
+    end
+  end
 end
