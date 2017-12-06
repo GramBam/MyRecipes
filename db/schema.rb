01a5c45a063a91adcd41b3b7406d8e632efa1815
GramBam <graham.moss@hotmail.com>
2017-12-06 02:48:19 -0500

    add associations between chef and recipes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171206061452) do
+ActiveRecord::Schema.define(version: 20171206074335) do
 
   create_table "chefs", force: :cascade do |t|
     t.string "chefname"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20171206061452) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "chef_id"
   end
 
 end
