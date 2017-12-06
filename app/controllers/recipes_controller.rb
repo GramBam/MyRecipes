5b836ebaa4598d5f0aa61ce42c26758850514202
GramBam <graham.moss@hotmail.com>
2017-12-06 14:29:23 -0500

    Add edit/create methods and view

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -17,10 +17,24 @@ class RecipesController < ApplicationController
     @recipe.chef = Chef.find(1)
 
     if  @recipe.save
-      flash[:success] = "Your recipe was creted successfully"
+      flash[:success] = "Your recipe was created successfully"
       redirect_to recipes_path
     else
       render :new
+    end
+  end
+
+  def edit
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+    if @recipe.update(recipe_params)
+      flash[:success] = "Recipe updated successfully"
+      redirect_to recipe_path(@recipe)
+    else
+      render :edit
     end
   end
 
