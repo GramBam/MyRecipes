a49467c8523262503078867214439209c576c3f4
GramBam <graham.moss@hotmail.com>
2017-12-06 23:01:37 -0500

    Build login/logout functionality

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+
+  def new
+
+  end
+
+  def create
+    chef = Chef.find_by(email: params[:email])
+    if chef && chef.authenticate(params[:password])
+      session[:chef_id] = chef.id
+      flash[:sucess] = "Logged in!"
+      redirect_to recipes_path
+    else
+      flash.now[:danger] = "Your email or password does not match"
+    end
+  end
+
+  def destroy
+    session[:chef_id] = nil
+    flash[:success] = "Logged out!"
+    redirect_to root_path
+  end
+
+end
