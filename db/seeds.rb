c3664f60c3ffc1c3a7b5d810504d7c6e11891162
GramBam <graham.moss@hotmail.com>
2017-12-12 01:54:58 -0500

    rename app, add favicon

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,5 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-chef = Chef.create!(chefname: 'Joe', email: 'joe@example.com', password: 'jjoJijO8u*O8o**18j28*&(*&@#^#hj89e*@J)', picture: Rails.root.join('app/assets/images/default.png').open)
-recipes = chef.recipes.create!([{ name: 'pesto', summary: "very tasty", description: "This will teach you how to make pesto", picture: Rails.root.join('app/assets/images/pesto.jpg').open}, { name: 'chicken', summary: "gosh darn delicious", description: "This will teach you how to make chicken", picture: Rails.root.join('app/assets/images/chicken.jpg').open }])
+chef = Chef.create!(chefname: 'Daisy', email: 'daisy@example.com', password: 'jjoJijO8u*O8o**18j28*&(*&@#^#hj89e*@J)', picture: Rails.root.join('app/assets/images/daisy.jpg').open)
+recipes = chef.recipes.create!([{ name: 'Pesto', summary: "Very delicious Pesto recipe!", description: "This will teach you how to make pesto", picture: Rails.root.join('app/assets/images/pesto.jpg').open}, { name: 'Chicken', summary: "gosh darn delicious", description: "This will teach you how to make chicken", picture: Rails.root.join('app/assets/images/chicken.jpg').open }])
