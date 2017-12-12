97423921d9a209a4f3211a533da3f336f13a22ea
GramBam <graham.moss@hotmail.com>
2017-12-12 17:29:00 -0500

    Add styles seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,1 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
-chef = Chef.create!(chefname: 'Daisy', email: 'daisy@example.com', password: 'jjoJijO8u*O8o**18j28*&(*&@#^#hj89e*@J)', picture: Rails.root.join('app/assets/images/daisy.jpg').open)
-recipes = chef.recipes.create!([{ name: 'Pesto', summary: "Very delicious Pesto recipe!", description: "This will teach you how to make pesto", picture: Rails.root.join('app/assets/images/pesto.jpg').open}, { name: 'Chicken', summary: "gosh darn delicious", description: "This will teach you how to make chicken", picture: Rails.root.join('app/assets/images/chicken.jpg').open }])
+styles = Style.create([{ name: 'Asian'}, {name: 'Brazillian'}, {name: 'Breakfast'}, {name: 'Caribbean'}, {name: 'Deli'}, {name: 'Dessert'}, {name: 'Ethiopian'}, {name: 'French'}, {name: 'German'}, {name: 'Greek'}, {name: 'Italian'}, {name: 'Jamaican'}, {name: 'Korean'}, {name: 'Lebanese'}, {name: 'Mediterranean'}, {name: 'Mexican'}, {name: 'Middle Eastern'}, {name: 'Moroccan'}, {name: 'Pan Asian'}, {name: 'Polish'}, {name: 'Portuguese'}, {name: 'Seafood'}, {name: 'Tex-Mex'}, {name: 'Vegan'}, {name: 'Vegatarian'}, {name: 'Vietnamese'}])
